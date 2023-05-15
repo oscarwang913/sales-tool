b8e4675790b312371d516fa30de8bfcea62113fc
DESKTOP-JM65FNB\oscar <garnett284@gmail.com>
2023-05-15 23:02:27 +0800

    test 6

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter basename={'/'}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
 )
